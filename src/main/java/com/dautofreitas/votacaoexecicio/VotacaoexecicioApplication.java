52c4cf1c6a4ab403e6be6eb2261df2c927086ccc
adauto freitas <dauto_freitas@hotmail.com>
2023-04-11 19:52:23 -0300

    Commit inicial

---
+++ src/main/java/com/dautofreitas/votacaoexecicio/VotacaoexecicioApplication.java
@@ -0,0 +1,13 @@
+package com.dautofreitas.votacaoexecicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoexecicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoexecicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dautofreitas/votacaoexecicio/VotacaoexecicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dautofreitas.votacaoexecicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoexecicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
